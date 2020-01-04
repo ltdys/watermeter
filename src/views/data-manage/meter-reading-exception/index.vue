e62322f91b013540bb3d4899d63c15e21152bb79
jiangdaye <692466154@qq.com>
2020-01-04 16:29:47 +0800

    添加统计分析和抄表统计图表

--- src/views/data-manage/meter-reading-exception/index.vue
+++ src/views/data-manage/meter-reading-exception/index.vue
@@ -1,186 +1,280 @@
 <template>
-  <div class="alarm">
+  <div class="meter-reading-exception">
     <el-row>
       <el-col :span="24">
         <el-form ref="search" :inline="true" :model="search" class="toolbar" size="mini">
-          <el-form-item :label="$t('dataManageException.toolbarA')">
-            <el-date-picker
-              v-model="search.t1"
-              type="datetime"
-              :placeholder="$t('dataManageException.toolbarA_')">
-            </el-date-picker>
+          <el-form-item label="组织">
+            <el-cascader
+              ref="cascader12"
+              v-model="search.orgList"
+              clearable
+              placeholder="请选择组织"
+              :options="treeData"
+              filterable
+              :props="setProps"
+              size="mini"
+              class="cascader12"
+              @change="changeOrg"
+              v-if="role_name === '超级管理员'"
+            />
           </el-form-item>
-          <el-form-item :label="$t('dataManageException.toolbarB')">
-            <el-date-picker
-              v-model="search.t2"
-              type="datetime"
-              :placeholder="$t('dataManageException.toolbarB_')">
-            </el-date-picker> 
+          <el-form-item label="区域">
+            <el-cascader
+              ref="cascader3"
+              class="cascader3"
+              placeholder="请选择区域"
+              size="mini"
+              v-model="search.areasList"
+              :options="list"
+              clearable
+              filterable
+              :props="setParent"
+              @change="changeParent"
+            />
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" icon="el-icon-search">{{ $t('common.query') }}</el-button>
-          </el-form-item>
-          <el-form-item>
-            <el-radio v-model="search.t3" label="1">{{ $t('dataManageException.toolbarRadioA') }}</el-radio>
-            <el-radio v-model="search.t3" label="2">{{ $t('dataManageException.toolbarRadioB') }}</el-radio>
-            <el-radio v-model="search.t3" label="3">{{ $t('dataManageException.toolbarRadioC') }}</el-radio>
+             <el-button type="primary" icon="el-icon-search" size="mini" @click.native="searchSubmit1()">查询</el-button>
           </el-form-item>
         </el-form>
       </el-col>
-      <el-col :span="4" :style="{height: (tableHeightPage + 52 - 30) + 'px', background: '#E9E9E9'}">
-        <el-scrollbar class="scrollbar-page" wrap-class="scrollbar-wrapper">
-          <my-region3 @handleNodeClick="handleNodeClick"></my-region3>
-        </el-scrollbar>
-      </el-col>
-      <el-col :span="20">
-        <el-table
-          :data="tableData"
-          border
-          stripe
-          style="width: 100%"
-          size="mini"
-          :height="tableHeightPage - 30"
-          highlight-current-row
-        >
-          <el-table-column
-            type="index"
-            width="50"
-            label="#"
-          />
-          <el-table-column
-            prop="region"
-            :label="$t('dataManageException.tableA')"
-          />
-          <el-table-column
-            prop="address"
-            :label="$t('dataManageException.tableB')"
-            width="120"
-          />
-          <el-table-column
-            prop="id"
-            :label="$t('dataManageException.tableC')"
-            width="220"
-          />
-          <el-table-column
-            prop="id"
-            :label="$t('dataManageException.tableD')"
-          />
-          <el-table-column
-            prop="num"
-            :label="$t('dataManageException.tableE')"
-          />
-          <el-table-column
-            prop="id"
-            :label="$t('dataManageException.tableF')"
-          />
-          <el-table-column
-            prop="name"
-            :label="$t('dataManageException.tableG')"
-          />
-          <el-table-column
-            prop="card"
-            :label="$t('dataManageException.tableH')"
-            width="160"
-          />
-        </el-table>
-        <my-pagination
-          :all-total="pageObj.allTotal"
-          :current-page="pageObj.currentPage"
-          :page-size="pageObj.pageSize"
-          :page-sizes="pageObj.pageSizes"
-          @pageChange="pageChange"
-          @currentChange="currentChange"
-        />
-      </el-col>
+        <el-col :span="24">
+          <el-scrollbar class="scrollbar-page" wrap-class="scrollbar-wrapper">
+            <div class="exception-wrap" :style="{ height: tableHeight + 'px'}">
+              <div class="exception-content" v-for="(item, index) in lnglatsList" :key="index">
+                <div class="text-ellipsis"><span :title="item.name">{{ item.name }}</span></div>
+                <div></div>
+                <div>总数：{{ item.meterNum }}</div>
+                <div :class="[item.meterNum - item.meterFailNum > 0 ? 'success-text' : '']">成功：{{ item.meterNum - item.meterFailNum }}</div>
+                <div :class="[item.meterFailNum > 0 ? 'fail-text' : '']">失败：{{ item.meterFailNum }}</div>
+                <div class="text-ellipsis">{{ item.name }}</div>
+              </div>
+            </div>
+          </el-scrollbar>
+        </el-col>
     </el-row>  
   </div>
 </template>
 
 <script>
-import { getException } from '@/service/dataManage'
-import myRegion3 from '@/components/common/region3'
-import myPagination from "@/components/pagination/my-pagination";
 import { list_mixins } from '@/mixins'
+import { orgTreeData, treeDataUtil, treeDataTest } from '@/utils/publicUtil'
+import { showDistrictInMap, findCompany, findDistrict } from "@/service/api";
+
 export default {
 
   name: 'alarm',
-
-  components: {
-    myPagination, myRegion3
-  },
 
   mixins: [list_mixins],
 
   data () {
     return {
+      lnglatsList: [],
+      companyId: "",
       tableData: [],
       search: {
-        t1: '',
-        t2: '',
-        t3: '1'
+        areasList: [],
+        areasId: '',
+        orgList: [],
+        org: ""
       },
-      pageObj: {
-        allTotal: 0, // 总条数
-        currentPage: 1, // 当前页数
-        pageSize: 50, // 每页条数
-        pageSizes: [10, 20, 50, 100]
+      setProps: { // 设置级联选择器
+        label: 'companyName',
+        value: 'id',
+        expandTrigger: 'click',
+        checkStrictly: true
       },
-      options: [{
-        value: 0,
-        label: '全部'
-      }, {
-        value: 1,
-        label: '其它'
-      }]
+      setParent: { // 设置级联选择器
+        label: 'name',
+        value: 'id',
+        expandTrigger: 'click',
+        checkStrictly: true
+      },
+      treeData: [],
+      tableDataFj: [],
+      list: []
     }
   },
 
   created () {
-    this.init()
+    this.companyId = this.company_id
+    this.findCompany('0')
+    this.findDistrict()
+  },
+
+  mounted() {
+    this.showDistrictInMap()
   },
 
   methods: {
     init () {
-      this.getException()
     },
-    async getException () {
-      const params = {
-        rows: this.pageObj.pageSize,
-        page: this.pageObj.currentPage
+     changeOrg () { // 组织机构选择
+      this.cascaderFalse('cascader12')
+      if(this.search.orgList && this.search.orgList.length > 0) {
+        this.search.org = this.search.orgList[this.search.orgList.length - 1]
+        this.companyId = this.search.org
+        this.findDistrict()
+      } else {
+        this.search.org = ""
+        this.companyId = this.company_id
       }
-      let res = await getException(params)
-      this.tableData = res.data.data
-      this.pageObj.allTotal = res.data.allTotal
     },
-    pageChange (data) { // 每页条数切换回调事件
-      this.pageObj.pageSize = data;
-      this.init()
+    async showDistrictInMap() {
+      let params = {
+        userId: this.userId,
+        companyId: this.companyId,
+        districtId: this.search.areasId
+      }
+      let resData = await showDistrictInMap(params)
+      if(resData.status === 200) {
+        this.regionList = resData.data.data
+        if(this.regionList.length > 0) {
+          this.regionList.forEach((item, index) => {
+            if(item.distList.length > 0) {
+              item.distList.forEach((item1, index1) => {
+                this.lnglatsList.push({
+                  companyName: item.company.companyName || "",
+                  latitude: item1.dist.latitude,
+                  longitude: item1.dist.longitude,
+                  name: item1.dist.name,
+                  address: item1.dist.address,
+                  meterConcentratorNum: item1.meterConcentratorNum,
+                  meterNodeNum: item1.meterNodeNum,
+                  meterNum: item1.meterNum,
+                  meterFailNum: item1.meterFailNum,
+                  id: item1.id,
+                  companyId: item1.companyId
+                })
+              })
+            }
+          })
+        }
+      }
     },
-    currentChange (data) { // 当前页切换事件
-      this.pageObj.currentPage = data;
-      this.init()
+    async findCompany (key) { // 获取组织机构
+      let params = {
+        userId: this.userId,
+        currentPage: 1,
+        pageSize: 10000,
+        company: {
+          id: this.companyId
+        }
+      }
+      let resData = await findCompany(params)
+      console.log('获取组织机构', resData)
+      if (resData.status === 200 && resData.data.data !== null) {
+        let list = resData.data.data
+        this.treeData = JSON.parse(orgTreeData([...list]))
+        if (key === '0') {
+          let param = {
+            companyId: this.treeData[0].id
+          }
+          // this.search.orgList = [this.treeData[0].id]
+          this.search.org = this.search.orgList[this.search.orgList.length - 1]
+        }
+      }
     },
-    handleNodeClick(data) {
+    async findDistrict () { // 查询区域
+      const self = this;
+      let param = {
+        companyId: this.companyId
+      }
+      let res = await findDistrict(param)
+      console.log('查询区域', res)
+      if (res.status === 200 && res.data.data !== null) {
+        let list = res.data.data || []
+        console.log('查询区域', list)
+        if (list.length !== 0) {
+          list = list.map(item => {
+            self.$set(item, 'parentId', item.parentid)
+            self.$set(item, 'companyId', item.companyid)
+            return item
+          })
+          list = list.filter(item => {
+            return item.state == 0
+          })
+          console.log(list)
+          self.$nextTick(() => {
+            self.tableDataFj = list
+            self.list = JSON.parse(treeDataUtil([...list], 'parentId', 'id'))
+          })
+        } else {
+          self.tableDataFj = list
+          self.list = list
+        }
+      }
     },
-    searchSubmit () {
-
+    changeParent () { // 区域选择、
+      this.cascaderFalse('cascader3')
+      if (this.search.areasList && this.search.areasList.length > 0) {
+        this.search.areasId = this.search.areasList[this.search.areasList.length - 1]
+      } else {
+        this.search.areasId = ""
+      }
     },
-    handleEdit () {
-
-    },
-    handleDelete () {
-
-    },
-    handleNodeClick (data) {
-      this.$message.success(`切换${data.label}成功`)
+    searchSubmit1() {
+      this.showDistrictInMap()
     }
   }
 }
 </script>
 
 <style lang="scss">
-  .alarm {
-    overflow-y: scroll;
+  .meter-reading-exception {
+    // overflow-y: scroll;
+    .exception-wrap {
+      width: 100%;
+      display: flex;
+      flex-wrap: wrap;
+      .exception-content {
+        border-radius: 10px;
+        width: 150px;
+        height: 180px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        background:rgba(250,250,250,1);
+        border:1px solid rgba(221,224,230,1);
+        margin-left: 10px;
+        margin-top: 10px;
+        >div:nth-child(1) {
+          color: #5B9CD0;
+          font-weight: 500;
+          font-size: 16px;
+        }
+        >div:nth-child(2) {
+          width: 90%;
+          height: 1px;
+          border-top: 1px solid #D8DBD7; 
+        }
+        >div:nth-child(3) {
+          margin-top: 10px;
+        }
+        >div:nth-child(6) {
+          width: 100%;
+          text-align: center;
+          height: 50px;
+          line-height: 50px;
+          background: #8FCFFB;
+          color: #fff;
+          font-weight: 500;
+        }
+        .success-text {
+          color: #57D755;
+        }
+        .fail-text {
+          color: red;
+        }
+        .text-ellipsis {
+          text-align: center;
+          width: 150px;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+        }
+      }
+    }
     .el-scrollbar__wrap {
       background: #fff;
     }
--- src/views/data-manage/statistical-analysis/index.vue
+++ src/views/data-manage/statistical-analysis/index.vue
@@ -1,260 +1,302 @@
 <template>
-  <div class="statistical-analysis">
+  <div class="meter-reading-exception">
     <el-row>
-      <el-col :span="4" :style="{height: (tableHeight + 32) + 'px', background: '#E9E9E9'}">
-        <el-scrollbar class="scrollbar-page" wrap-class="scrollbar-wrapper">
-          <my-region3 @handleNodeClick="handleNodeClick" />
-        </el-scrollbar>
-      </el-col>
-      <el-col :span="20" class="statistical-analysis_wrap" :style="{height: (tableHeight + 32) + 'px', background: '#fff'}">
-        <div class="statistical-analysis_wrap__title">
-          <div v-for="(item, index) in statisticsList" :key="index" :class="[item.isCheck ? 'is-check' : 'is-default']" @click="conditionChange(item)">
-            {{ item.label }}
-          </div>
-        </div>
-        <div class="statistical-analysis_wrap__condition" :class="[areaObj.type != 2 ? '' : areaObj.area == 'sb' ? 'al3' : 'al2']">
-          <div v-if="areaObj.type == 2" class="wrap_box">
-            <span>选择类型</span>
-            <el-select v-model="areaObj.area" clearable placeholder="请选择类型">
-              <el-option
-                v-for="item in areaList"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              />
-            </el-select>
-          </div>
-          <div v-if="areaObj.type == 2 && areaObj.area == 'sb'" class="wrap_box">
-            <span>选择用户</span>
-            <el-select v-model="areaObj.user" clearable placeholder="请选择用户">
-              <el-option
-                v-for="item in userList"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              />
-            </el-select>
-          </div>
-          <div class="wrap_box">
-            <span>{{ $t('dataManageAnalysis.toolbarD') }}</span>
-            <el-date-picker
-              v-model="value1"
-              type="date"
-              :placeholder="$t('dataManageAnalysis.toolbarD_')"
+      <el-col :span="24">
+        <el-form ref="search" :inline="true" :model="search" class="toolbar" size="mini">
+          <el-form-item label="组织">
+            <el-cascader
+              ref="cascader12"
+              v-model="search.orgList"
+              clearable
+              placeholder="请选择组织"
+              :options="treeData"
+              filterable
+              :props="setProps"
+              size="mini"
+              class="cascader12"
+              @change="changeOrg"
+              v-if="role_name === '超级管理员'"
             />
-          </div>
-        </div>
-        <div class="statistical-analysis_wrap__charttitle">
-          <div>{{ $t('dataManageAnalysis.chartTitleA') }}</div>
-          <div>{{ $t('dataManageAnalysis.chartTitleB') }}</div>
-        </div>
-        <div class="statistical-analysis_wrap__chart">
-          <water-chart width="45%" height="100%" />
-          <loss-chart width="45%" height="100%" />
-        </div>
-
+          </el-form-item>
+          <el-form-item label="区域">
+            <el-cascader
+              ref="cascader3"
+              class="cascader3"
+              placeholder="请选择区域"
+              size="mini"
+              v-model="search.areasList"
+              :options="list"
+              clearable
+              filterable
+              :props="setParent"
+              @change="changeParent"
+            />
+          </el-form-item>
+          <el-form-item label="统计方式">
+            <el-select v-model="search.way" placeholder="请选择" @change="wayChange">
+              <el-option
+                v-for="item in wayOptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+            <el-select v-model="search.cond" placeholder="请选择">
+              <el-option
+                v-for="item in condOptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item>
+             <el-button type="primary" icon="el-icon-search" size="mini" @click.native="searchSubmit1()">查询</el-button>
+          </el-form-item>
+        </el-form>
       </el-col>
-    </el-row>
+        <el-col :span="24">
+          <el-scrollbar class="scrollbar-page" wrap-class="scrollbar-wrapper">
+            <div class="exception-wrap" :style="{ height: tableHeight + 'px'}">
+              抄表用水图表
+            </div>
+          </el-scrollbar>
+        </el-col>
+    </el-row>  
   </div>
 </template>
 
 <script>
-import myRegion3 from '@/components/common/region3'
-import waterChart from './water-chart'
-import lossChart from './loss-chart'
 import { list_mixins } from '@/mixins'
+import { orgTreeData, treeDataUtil, treeDataTest } from '@/utils/publicUtil'
+import { showDistrictInMap, findCompany, findDistrict } from "@/service/api";
+
 export default {
 
-  name: 'statisticalAnalysis',
-
-  components: {
-    myRegion3, waterChart, lossChart
-  },
+  name: 'alarm',
 
   mixins: [list_mixins],
 
   data () {
     return {
-      fileList: [],
-      statisticsList: [{
-        label: this.$t('dataManageAnalysis.toolbarA'),
-        value: 0,
-        isCheck: true
-      }, {
-        label: this.$t('dataManageAnalysis.toolbarB'),
-        value: 1,
-        isCheck: false
-      }, {
-        label: this.$t('dataManageAnalysis.toolbarC'),
-        value: 2,
-        isCheck: false
-      }],
-      value1: '',
-      areaObj: {
-        type: 0, // 0  默认  1  组织  2  区域
-        area: '', // 选择值
-        user: '' // 选择用户
+      lnglatsList: [],
+      companyId: "",
+      tableData: [],
+      search: {
+        areasList: [],
+        areasId: '',
+        orgList: [],
+        org: "",
+        way: "",
+        cond: ""
       },
-      areaList: [
-        {
-          label: '大表',
-          value: 'db'
-        }, {
-          label: '集中器',
-          value: 'jzq'
-        }, {
-          label: '水表',
-          value: 'sb'
-        }
-      ],
-      userList: [
-        {
-          label: '用户1',
-          value: 'yh1'
-        }, {
-          label: '用户2',
-          value: 'yh2'
-        }, {
-          label: '用户3',
-          value: 'yh3'
-        }, {
-          label: '用户4',
-          value: 'yh4'
-        }, {
-          label: '用户5',
-          value: 'yh5'
-        }, {
-          label: '用户6',
-          value: 'yh6'
-        }, {
-          label: '用户7',
-          value: 'yh7'
-        }, {
-          label: '用户8',
-          value: 'yh8'
-        }
-      ]
+      setProps: { // 设置级联选择器
+        label: 'companyName',
+        value: 'id',
+        expandTrigger: 'click',
+        checkStrictly: true
+      },
+      setParent: { // 设置级联选择器
+        label: 'name',
+        value: 'id',
+        expandTrigger: 'click',
+        checkStrictly: true
+      },
+      treeData: [],
+      tableDataFj: [],
+      list: [],
+      wayOptions: [{
+        label: "按年统计",
+        value: 1
+      }, {
+        label: "按月统计",
+        value: 2
+      }, {
+        label: "按季统计",
+        value: 3
+      }],
+      condOptions: []
     }
   },
 
   created () {
+    this.companyId = this.company_id
+    this.findCompany('0')
+    this.findDistrict()
+  },
+
+  mounted() {
+    this.showDistrictInMap()
   },
 
   methods: {
     init () {
     },
-    handleNodeClick (data) {
-      console.log('data', data)
-      if (data.hasOwnProperty('companyid')) { // 区域
-        this.areaObj.type = '2'
-      } else { // 组织
-        this.areaObj.type = '1'
+     changeOrg () { // 组织机构选择
+      this.cascaderFalse('cascader12')
+      if(this.search.orgList && this.search.orgList.length > 0) {
+        this.search.org = this.search.orgList[this.search.orgList.length - 1]
+        this.companyId = this.search.org
+        this.findDistrict()
+      } else {
+        this.search.org = ""
+        this.companyId = this.company_id
       }
-      // this.$message.success(`切换${data.label}成功`)
     },
-    submitUpload () {
-      this.$refs.upload.submit();
+    async showDistrictInMap() {
+      let params = {
+        userId: this.userId,
+        companyId: this.companyId,
+        districtId: this.search.areasId
+      }
+      let resData = await showDistrictInMap(params)
+      if(resData.status === 200) {
+        this.regionList = resData.data.data
+        if(this.regionList.length > 0) {
+          this.regionList.forEach((item, index) => {
+            if(item.distList.length > 0) {
+              item.distList.forEach((item1, index1) => {
+                this.lnglatsList.push({
+                  companyName: item.company.companyName || "",
+                  latitude: item1.dist.latitude,
+                  longitude: item1.dist.longitude,
+                  name: item1.dist.name,
+                  address: item1.dist.address,
+                  meterConcentratorNum: item1.meterConcentratorNum,
+                  meterNodeNum: item1.meterNodeNum,
+                  meterNum: item1.meterNum,
+                  meterFailNum: item1.meterFailNum,
+                  id: item1.id,
+                  companyId: item1.companyId
+                })
+              })
+            }
+          })
+        }
+
+      }
     },
-    handleRemove (file, fileList) {
-      console.log(file, fileList);
+    async findCompany (key) { // 获取组织机构
+      let params = {
+        userId: this.userId,
+        currentPage: 1,
+        pageSize: 10000,
+        company: {
+          id: this.companyId
+        }
+      }
+      let resData = await findCompany(params)
+      console.log('获取组织机构', resData)
+      if (resData.status === 200 && resData.data.data !== null) {
+        let list = resData.data.data
+        this.treeData = JSON.parse(orgTreeData([...list]))
+        if (key === '0') {
+          let param = {
+            companyId: this.treeData[0].id
+          }
+          // this.search.orgList = [this.treeData[0].id]
+          this.search.org = this.search.orgList[this.search.orgList.length - 1]
+        }
+      }
     },
-    handlePreview (file) {
-      console.log(file);
+    async findDistrict () { // 查询区域
+      const self = this;
+      let param = {
+        companyId: this.companyId
+      }
+      let res = await findDistrict(param)
+      console.log('查询区域', res)
+      if (res.status === 200 && res.data.data !== null) {
+        let list = res.data.data || []
+        console.log('查询区域', list)
+        if (list.length !== 0) {
+          list = list.map(item => {
+            self.$set(item, 'parentId', item.parentid)
+            self.$set(item, 'companyId', item.companyid)
+            return item
+          })
+          list = list.filter(item => {
+            return item.state == 0
+          })
+          console.log(list)
+          self.$nextTick(() => {
+            self.tableDataFj = list
+            self.list = JSON.parse(treeDataUtil([...list], 'parentId', 'id'))
+          })
+        } else {
+          self.tableDataFj = list
+          self.list = list
+        }
+      }
     },
-    conditionChange (value) {
-      this.statisticsList.forEach((item, index) => {
-        item.isCheck = item.label === value.label
-      })
+    changeParent () { // 区域选择、
+      this.cascaderFalse('cascader3')
+      if (this.search.areasList && this.search.areasList.length > 0) {
+        this.search.areasId = this.search.areasList[this.search.areasList.length - 1]
+      } else {
+        this.search.areasId = ""
+      }
+    },
+    searchSubmit1() {
+      this.showDistrictInMap()
+    },
+    wayChange() {
+      this.condOptions = []
+      this.search.cond = ""
+      if (this.search.way === 1) {  // 按年
+        let year = parseInt(new Date().getFullYear());
+        console.log("当前年份", year)
+        for(let i = 0; i < 5; i ++) {
+          this.condOptions.push({
+            label: year + "年",
+            value: year
+          })
+          year--
+        }
+      } else if(this.search.way === 2) {  // 按月
+        for(let i = 1; i < 13; i++) {
+          this.condOptions.push({
+            label: i + "月",
+            value: i
+          })
+        }
+      } else if(this.search.way === 3) {  // 按季度
+        this.condOptions = [{
+          label: "第一季度",
+          value: 1
+        }, {
+          label: "第二季度",
+          value: 2
+        }, {
+          label: "第三季度",
+          value: 3
+        }, {
+          label: "第四季度",
+          value: 4
+        }]
+      }
     }
   }
 }
 </script>
 
 <style lang="scss">
-  .statistical-analysis {
-    position: relative;
-    overflow: hidden;
+  .meter-reading-exception {
+    // overflow-y: scroll;
+    .exception-wrap {
+      width: 100%;
+      display: flex;
+      flex-wrap: wrap;
+    }
     .el-scrollbar__wrap {
       background: #fff;
     }
-    &_wrap {
-      &__title {
-        display: flex;
-        justify-content: center;
-        > div {
-          margin-top: 30px;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          width: 22.22%;
-          height: 50px;
-          border: 1px solid #E2E5EB;
-          cursor: pointer;
-        }
-        div:nth-child(2) {
-          border-left: 0px;
-          border-right: 0px;
-        }
+    .toolbar{
+      height:auto;
+      margin-bottom: 10px;
+      .el-form-item {
+        margin-bottom: 2.5px;
       }
-      &__condition {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin: 20px 0px;
-        > span {
-          font-family:PingFang-SC-Heavy,PingFang-SC;
-          font-weight: 400;
-          color: #292C33;
-          margin-right: 10px;
-        }
-      }
-      .al2{
-        .wrap_box {
-          width: 30%;
-        }
-      }
-      .al3{
-        justify-content: left;
-        padding-left: 50px;
-        box-sizing: border-box;
-        .wrap_box{
-          margin-right: 50px;
-        }
-      }
-      &__charttitle {
-        display: flex;
-        justify-content: space-evenly;
-        div {
-          width: 45%;
-          height: 50px;
-          box-sizing: border-box;
-          line-height: 50px;
-          background: #f5f5f5;
-          font-family:FZLTTHK--GBK1-0,FZLTTHK--GBK1;
-          font-weight:600;
-          color: #292c33;
-          padding-left: 20px;
-        }
-      }
-      &__chart {
-        display: flex;
-        justify-content: space-evenly;
-        align-items: center;
-        height: 71%;
-      }
-    }
-    .is-check {
-      background: #383F52;
-      border-radius: 4px 0px 0px 4px;
-      color: #fff;
-      font-weight:normal;
-      font-family:FZLTTHK--GBK1-0,FZLTTHK--GBK1;
-    }
-    .is-default {
-      background: transparent;
-      border-radius: 0px;
-      font-weight:normal;
-      font-family:PingFang-SC-Heavy,PingFang-SC;
     }
   }
 </style>
