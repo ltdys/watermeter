1dbe5a9336503c92ae4a598637f354d4e94ad0a1
wangguishen <18537072262@163.com>
2020-04-08 22:39:14 +0800

    修改编辑器bug

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -10,7 +10,6 @@ Vue.prototype.TABLE_SIZE = 'mini'
 
 // Vue.prototype.DOWNLOAD_METER_READ = "http://120.77.248.38:8099/watermeter/Meter_Read.xls"
 
-
 Vue.prototype.LOGO_URL = 'http://101.200.224.248:8099/watermeter/'
 
 Vue.prototype.DOWNLOAD_USER_EXCEL = "http://101.200.224.248:8099/watermeter/Meter_User_NB.xls"
